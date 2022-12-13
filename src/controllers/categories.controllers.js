82047c348124ff75a5191d7ef28fd0c86fdfa59a
Júlia Botelho <cajujumo@gmail.com>
2022-12-13 12:46:56 -0300

    feat: final version.2

--- src/controllers/categories.controllers.js
+++ src/controllers/categories.controllers.js
@@ -3,26 +3,31 @@ import { connectionDB } from "../database/db.js"
 export async function createCat(req, res) {
     const { name } = req.body;
 
-    if (name === null || name === ""){
+    if (name === null || name === "") {
         res.sendStatus(400)
         return;
     }
 
     try {
-      await connectionDB.query("INSERT INTO categories (name) VALUES ($1);", [name]); 
+        const catExists = await connectionDB.query('SELECT * FROM  categories WHERE name=$1;', [name])
+        if (catExists.rows.length > 0) {
+            res.sendStatus(409)
+            return;
+        }
+        await connectionDB.query("INSERT INTO categories (name) VALUES ($1);", [name]);
 
-      res.sendStatus(201);
+        res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message);
     }
 }
 
 export async function findAllCats(req, res) {
-    try{
+    try {
         const allCategories = await connectionDB.query("SELECT * FROM categories;");
 
         res.send(allCategories.rows);
-    }catch (err) {
+    } catch (err) {
         res.status(500).send(err.message);
     }
 }
